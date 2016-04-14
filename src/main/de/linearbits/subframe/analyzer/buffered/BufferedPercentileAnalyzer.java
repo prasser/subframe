087cf48e73f97f44eafe463a8b5a8436fca6e41e
Fabian Prasser <mail@fabian-prasser.de>
2016-04-14 17:33:25 +0200

    Update license headers

--- src/main/de/linearbits/subframe/analyzer/buffered/BufferedPercentileAnalyzer.java
+++ src/main/de/linearbits/subframe/analyzer/buffered/BufferedPercentileAnalyzer.java
@@ -1,6 +1,6 @@
 /*
  * SUBFRAME - Simple Java Benchmarking Framework
- * Copyright (C) 2012 - 2013 Fabian Prasser
+ * Copyright (C) 2012 - 2016 Fabian Prasser and contributors
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
