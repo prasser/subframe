2e6e489088b044d8e1552f9703c05301a2c2857a
Johanna Eicher <eicherj@users.noreply.github.com>
2018-01-08 17:19:42 +0100

    Introduce param 'omitEmptyBuckets' for SeriesBoxAndWhisker (#11)

--- src/main/de/linearbits/subframe/graph/SeriesBoxAndWhisker.java
+++ src/main/de/linearbits/subframe/graph/SeriesBoxAndWhisker.java
@@ -47,6 +47,25 @@ public class SeriesBoxAndWhisker extends Series<PointBoxAndWhisker> {
      * @param yField
      * @param interval
      * @param precision
+     * @param omitEmptyBuckets
+     */
+    public SeriesBoxAndWhisker(CSVFile file, 
+                               Field xField, 
+                               Field yField,
+                               double interval,
+                               int precision,
+                               boolean omitEmptyBuckets){
+        this(file, getDefaultSelector(), xField, yField, interval, precision, omitEmptyBuckets);
+    }
+
+    /**
+     * Creates a series by selecting rows and taking two values
+     * 
+     * @param file
+     * @param xField
+     * @param yField
+     * @param interval
+     * @param precision
      */
     public SeriesBoxAndWhisker(CSVFile file, 
                                Field xField, 
@@ -54,6 +73,41 @@ public class SeriesBoxAndWhisker extends Series<PointBoxAndWhisker> {
                                double interval,
                                int precision){
         this(file, getDefaultSelector(), xField, yField, interval, precision);
+    }
+    
+    /**
+     * Creates a series by selecting rows and taking two values
+     * 
+     * @param file
+     * @param selector
+     * @param xField
+     * @param yField
+     * @param interval
+     * @param precision
+     * @param omitEmptyBuckets
+     * @throws IOException 
+     */
+    public SeriesBoxAndWhisker(CSVFile file, 
+                               Selector<String[]> selector, 
+                               Field xField, 
+                               Field yField,
+                               double interval,
+                               int precision,
+                               boolean omitEmptyBuckets) {
+        
+        CSVFile input = file.getBucketizedFile(selector, xField.category, xField.measure, yField.category, yField.measure, interval, precision, omitEmptyBuckets);
+        Iterator<CSVLine> iter = input.iterator();
+        int id = 1;
+        while (iter.hasNext()) {
+            CSVLine csvline = iter.next();
+            data.add(new PointBoxAndWhisker(String.valueOf(id++),
+                                            csvline.get(yField.category, Analyzer.MINIMUM),
+                                            csvline.get(yField.category, Analyzer.PERCENTILE(0.25d)),
+                                            csvline.get(yField.category, Analyzer.MEDIAN),
+                                            csvline.get(yField.category, Analyzer.PERCENTILE(0.75d)),
+                                            csvline.get(yField.category, Analyzer.MAXIMUM),
+                                            csvline.get(xField.category, Analyzer.VALUE)));
+        }
     }
     
     /**
@@ -74,18 +128,6 @@ public class SeriesBoxAndWhisker extends Series<PointBoxAndWhisker> {
                     double interval,
                     int precision) {
         
-        CSVFile input = file.getBucketizedFile(selector, xField.category, xField.measure, yField.category, yField.measure, interval, precision);
-        Iterator<CSVLine> iter = input.iterator();
-        int id = 1;
-        while (iter.hasNext()) {
-            CSVLine csvline = iter.next();
-            data.add(new PointBoxAndWhisker(String.valueOf(id++),
-                                            csvline.get(yField.category, Analyzer.MINIMUM),
-                                            csvline.get(yField.category, Analyzer.PERCENTILE(0.25d)),
-                                            csvline.get(yField.category, Analyzer.MEDIAN),
-                                            csvline.get(yField.category, Analyzer.PERCENTILE(0.75d)),
-                                            csvline.get(yField.category, Analyzer.MAXIMUM),
-                                            csvline.get(xField.category, Analyzer.VALUE)));
-        }
+       this(file, selector, xField, yField, interval, precision, false);
     }
 }
